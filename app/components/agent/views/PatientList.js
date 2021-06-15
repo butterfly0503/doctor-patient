0c8a3170757970ace5ff1c752524ba7a164ecdad
butterfly0503 <dkope451@gmail.com>
2021-06-15 17:56:07 +0900

    remove react-native-gradient-button

--- app/components/agent/views/PatientList.js
+++ app/components/agent/views/PatientList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View, Button } from 'react-native';
-import GradientButton from 'react-native-gradient-buttons';
 
 import { MainHeader, MainHeaderRight, MainHeaderTitle } from '../../../globals/components/BTHeaders';
 
@@ -14,20 +13,6 @@ const PatientList = ({ navigation }) => {
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor:'white' }}>
                 <Button onPress={() => navigation.goBack()} title="Go back home" />
             </View>
-            <GradientButton
-                style={{ marginVertical: 8 }}
-                text="Gradient Button #1"
-                textStyle={{ fontSize: 20 }}
-                gradientBegin="#874f00"
-                gradientEnd="#f5ba57"
-                gradientDirection="diagonal"
-                height={60}
-                width={300}
-                radius={15}
-                impact
-                impactStyle='Light'
-                onPressAction={() => alert('You pressed me!')}
-            />
         </>
     );
 };
